37644b2b92b92564dc0be5801fa1b3e91dcacb01
Satria Primatama <100823016+satriaprimatama20@users.noreply.github.com>
2023-12-24 17:16:13 +0700

    Add files via upload

---
+++ dicoding-webcaffee/script.js
@@ -0,0 +1,16 @@
+// Toogle class active
+const navbarNav = document.querySelector("ul");
+
+// Ketika icon #menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan nav
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
